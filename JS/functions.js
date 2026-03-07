94368890d808b4eb0f0085a3d357fc267ac1ba50
EmanShabaan123 <shabaaneman46@gmail.com>
2026-03-08 00:44:18 +0200

    Add files via upload

---
+++ JS/functions.js
@@ -0,0 +1,286 @@
+function ChangeMainColor(colorName){
+    let HTML = document.querySelector("html"),
+        NewColor = getComputedStyle(HTML).getPropertyValue(`--${colorName}`);
+
+
+    TitleImage.forEach(function(CommonName){
+        ChangeMainImg(colorName.split("-")[0] ,CommonName, 'correct' );
+    })
+    ChangeMainImg(colorName.split("-")[0] , Logo , 'logo');
+
+   
+    HTML.style.setProperty("--main-color",NewColor);
+ 
+}
+// ///////////////////////////////////////////
+function ChangeMainImg(imgName , ImgEle , CommonName){
+    let CurrentImgSource = ImgEle.src ;
+    ImgEle.src =  ChangeSourse(CurrentImgSource , `${imgName}-${CommonName}.png` );
+    LogoIcon.href =  ChangeSourse(CurrentImgSource , `${imgName}-${CommonName}.png` )
+}
+
+// ///////////////////////////////////////////
+function OpenPopup(popupName){
+    let PopupEle = document.querySelector(`.pop-up[data-popup-name=${popupName}]`);
+
+    PopupEle.classList.add("active")
+    GloabalCarouser.classList.add("blur-header");
+     Offer.classList.add("blur-header");
+    LatestBlur.classList.add("blur-header");
+    Featured.classList.add("blur-header");
+    NavItem.classList.add("blur-header");
+
+    setTimeout(function(){
+    PopupEle.classList.add("show")
+    },100)
+}
+// ///////////////////////////////////////////
+function ClosePopup(){
+    let CurrentPopup = document.querySelector(".pop-up.active");
+        CurrentPopup.classList.remove("show");
+
+    setTimeout(function(){
+    CurrentPopup.classList.remove("active")
+     GloabalCarouser.classList.remove("blur-header");
+      Offer.classList.remove("blur-header");
+    LatestBlur.classList.remove("blur-header");
+    Featured.classList.remove("blur-header");
+    NavItem.classList.remove("blur-header");
+    },500)
+}
+// //////////////////////////////////////////
+function ShowImages(listImages , FromPopup = false){
+    let ImagesEle  = `` ;
+
+    listImages.forEach(function(productImg , index){
+        ImagesEle += `
+         <li onclick="ChangeSelectImage(this , '${productImg}')" class="${(!FromPopup)? 'mainborder rounded-2' : ''}  p-1 ${(index != listImages.length -1 )?"mb-lg-2  me-2" : ""}">
+         <img src="./images/products/${productImg}" alt="" class="img-fluid">
+         </li>
+        `;
+    })
+
+    return ImagesEle ;
+}
+// /////////////////////////////////
+function showprice(price , discount){
+   return `
+   <span class="maincolor text-decoration-line-through ${(discount == 0) ? 'd-none' : ' '}">${price}<sup>$</sup></span>
+    <span>${(price * (1-discount)).toFixed(2)}<sup>$</sup></span>
+   `;
+}
+
+// /////////////////////////////////
+function ShowSizes(listSizes , ProductIntoCart){
+    let SizeEle  = `` ;
+
+    listSizes.forEach(function(listSize , index){
+        if(ProductIntoCart == undefined){
+              SizeEle += `
+           <li onclick="ChangeActive(this); UpdateSize(this ,'${listSize}')
+           " class="${(index != listSizes.length -1 )?"me-2" : ""} ${(index == 0)?"active" : ""}  mainbutton ">
+           ${listSize}</li>
+        `;
+        }
+        else{
+        SizeEle += `
+           <li onclick="ChangeActive(this); UpdateSize(this ,'${listSize}')
+           " class="${(index != listSizes.length -1 )?"me-2" : ""} ${(ProductIntoCart.size == listSize)?"active" : ""}  mainbutton ">
+           ${listSize}</li>
+        `;
+        }
+      
+    })
+
+    return SizeEle ;
+}
+// ///////////////////////////
+function ShowImgIcon(listIcons){
+    let IconEle  = `` ;
+
+    listIcons.forEach(function(productImg , index){
+        IconEle += `
+             <li onclick="ChangeSelectImage(this, '${productImg}') ; ChangeActive(this)"
+              class="${(index != listIcons.length -1 )?"me-2" : ""} mainbutton ${(index == 0)?"active" : ""} rounded-circle"></li>
+        `;
+    })
+
+    return IconEle ;
+}
+// ///////////////////////////
+function ChangeSourse(mainsrc , newimg){
+    let MainArr = mainsrc.split('/');
+
+    MainArr[MainArr.length -1 ] = newimg ;
+    return MainArr.join('/')
+        
+}
+// ///////////////////////////
+function ChangeSelectImage(that , SelectedImg){
+      let ProductEle = that.closest(".product"),
+          SelectedImgEle = ProductEle.querySelector(".selsectedimg img");
+        
+    SelectedImgEle.src = ChangeSourse(SelectedImgEle.src , SelectedImg);
+}
+// //////////////////////////////////
+function ChangeActive(that){
+    that.parentElement.querySelector('.active').classList.remove('active');
+    that.classList.add('active');
+}
+// //////////////////////////
+function showDiscount(discount){
+    let DiscountPersentage = ``;
+
+    if(discount){
+        DiscountPersentage = `<h6 class="discount">-${discount*100}%</h6>`
+    }
+
+    return DiscountPersentage ;
+}
+
+////////////////////////////////
+function GetProduct(ProductId){
+    return products.find(product => product.id == ProductId) ;
+}
+///////////////////////////////
+function ShowListColors(listColors , ProductIntoCart){
+    let ColorEle  = `` ;
+  
+    listColors.forEach(function(ProductColor , index){
+        if(ProductIntoCart == undefined){
+           ColorEle += `
+           <li onclick="ChangeActive(this); UpdateColor(this, '${ProductColor}')" class="${(index != listColors.length -1 )?"me-2" : ""} ${(index == 0)?"active" : ""}  mainbutton "
+           style="background-color: ${ProductColor}"> </li>
+        `;
+        }else{
+            ColorEle += `
+           <li onclick="ChangeActive(this); UpdateColor(this, '${ProductColor}')" class="${(index != listColors.length -1 )?"me-2" : ""} ${(ProductColor == ProductIntoCart.color)?"active" : ""}  mainbutton "
+           style="background-color: ${ProductColor}"> </li>
+        `;
+        }
+      
+    })
+
+    return ColorEle ;
+
+}
+// /////////////////////////////
+function OpenProductPopup(ProductId){
+     
+    let product = GetProduct(ProductId),
+        PopupBoxElement = document.querySelector(`.pop-up[data-popup-name="product"] .box`),
+        ProductIntoCart = ProductCarts.find((item) => item.id == product.id);
+
+    PopupBoxElement.innerHTML = `
+      <div class="row product"
+        data-product-id ="${product.id}"
+        data-selected-color="${(ProductIntoCart == undefined) ? product.colors[0] : ProductIntoCart.color}"
+        data-selected-size="${(ProductIntoCart == undefined) ? product.sizes[0] : ProductIntoCart.size}">
+                  <div class="col-lg-6">
+                    <div class="item part1">
+                       <div class="selsectedimg">
+                          <img src="./images/products/${product.images[0]}" alt="" class="img-fluid ">
+                       </div>
+                      <div class="imglist">
+                        <ul class="list-unstyled d-flex">
+                          ${ShowImages(product.images , true )}
+                        </ul>
+                      </div>
+                    </div>
+                  </div>
+
+                  <div class="col-lg-6">
+                    <div class="item">
+                       <h2 class="maincolor">${product.name}</h2>
+                       ${showprice(product.price , product.discount)}
+                       <hr>
+
+                       <p>${product.description}</p>
+                         <div class="d-flex fw-bold">
+                          <div class="label me-2 ">
+                               <h5>Size :</h5>
+                          </div>
+                          <div class="value">
+                               <ul class="list-unstyled d-flex">
+                                    ${ShowSizes(product.sizes , ProductIntoCart)}
+                               </ul>
+                          </div>
+                        </div>
+
+
+                        <div class="d-flex fw-bold color">
+                          <div class="label me-2 ">
+                               <h5>Colors :</h5>
+                          </div>
+                          <div class="value">
+                               <ul class="list-unstyled d-flex">
+                                    ${ShowListColors(product.colors)}
+                               </ul>
+                          </div>
+                        </div>
+                      ${PrepareBtn(ProductIntoCart , product.id)}
+                    </div>
+                  </div>
+            </div>
+    `;
+    
+    OpenPopup("product");
+}
+///////////////////////////////////
+function AddToCart(that , ProductId){
+    let product = GetProduct(ProductId),
+        ProductEle = document.querySelector(`.product[data-product-id ="${product.id}"]`),
+        NewOrder = {
+            id : product.id ,
+            color : ProductEle.getAttribute('data-selected-color'),
+            size : ProductEle.getAttribute('data-selected-size'),
+        }
+    ProductCarts.push(NewOrder);
+    UpdateLocalStorage()
+    ToggleCartBtn(that , 'remove');
+    that.setAttribute('onclick', `RemoveFromCart(this , ${product.id})`);
+}
+///////////////////////////////////////
+function RemoveFromCart(that , ProductId){
+    ProductCarts = ProductCarts.filter((product) => product.id != ProductId);
+    UpdateLocalStorage()
+    ToggleCartBtn(that , 'add');
+
+     that.setAttribute('onclick', `AddToCart(this , ${ProductId})`);
+    
+}
+// ///////////////////////////////////
+function ToggleCartBtn(btn , status){
+    if(status == 'add'){
+        btn.textContent = "Add To Cart ";
+        btn.classList.remove('remove');
+    }else if(status == 'remove'){
+        btn.textContent = "Remove From Cart ";
+        btn.classList.add('remove'); 
+    }
+}
+// //////////////////////////////////
+function UpdateSize(that , newsize){
+     let productEle = that.closest(".product");
+
+    productEle.setAttribute("data-selected-size",newsize)
+}
+// //////////////////////////////////
+function UpdateColor(that , NewColor){
+     let productEle = that.closest(".product");
+
+    productEle.setAttribute("data-selected-color",NewColor)
+}
+// ////////////////////////////
+function UpdateLocalStorage(){
+    localStorage.setItem("ProductCarts" , JSON.stringify(ProductCarts));
+}
+// /////////////////////////
+function PrepareBtn(ProductIntoCart , ProductId){
+    if( ProductIntoCart == undefined){
+        return  `   <button class="btn mainbutton" onclick="AddToCart(this ,${ProductId})"  >Add To Cart</button>` 
+               
+    }else{
+        return   `<button class="btn mainbutton  " onclick="RemoveFromCart(this ,${ProductId})"  >Remove From Cart</button>`
+    }
+}
---
+++ JS/index.js
@@ -0,0 +1,222 @@
+let GloabalCarouser = document.querySelector("#EM-carousel"),
+    NextButton = GloabalCarouser.querySelector("button.next"),
+    PrevButton = GloabalCarouser.querySelector("button.prev"),
+    CarouselSliders = GloabalCarouser.querySelectorAll(".EM-item"),
+    NavToggler = document.querySelector(".navbar-toggler"),
+    NavMenu = document.querySelector("#navbarSupportedContent"),
+    NavItem = document.querySelector("nav"),
+    NavHeight = NavItem.clientHeight ,
+    TitleImage = document.querySelectorAll("section .title img"),
+    LatestBlur = document.querySelector("#Latest"),
+    Featured = document.querySelector("#Featured"),
+    Offer = document.querySelector("#Offer"),
+    NavLinks = document.querySelectorAll(".nav-link"),
+    LoadingPage = document.querySelector("#LoadingPage"),
+    PopupBoxes = document.querySelectorAll(".pop-up .box"),
+    LatestContent = document.querySelector("#Latest .content"),
+    FeaturedContent = document.querySelector("#Featured .content .row"),
+    ProductCarts = [];
+
+// ////////////////////////////////////
+if (localStorage.getItem("ProductCarts") == null){
+    UpdateLocalStorage();
+}else{
+    ProductCarts = JSON.parse(localStorage.getItem("ProductCarts"))
+}
+// ///////////////////////////////////////
+NextButton.addEventListener("click",function(){
+    let CurrentSlide = GloabalCarouser.querySelector(".EM-item.active"),
+        NextSlide = CurrentSlide.nextElementSibling ?? GloabalCarouser.querySelector(".EM-item:first-child");
+        CurrentColorName = NextSlide.dataset.colorName ;
+
+    ChangeMainColor(CurrentColorName);
+    CurrentSlide.classList.remove("active");
+    NextSlide.classList.add("active");
+})
+// ////////////////////////////////////////////////////////
+PrevButton.addEventListener("click",function(){
+    let CurrentSlide = GloabalCarouser.querySelector(".EM-item.active"),
+        prevSlide = CurrentSlide.previousElementSibling ?? GloabalCarouser.querySelector(".EM-item:last-child");
+        CurrentColorName = prevSlide.dataset.colorName ;
+
+    ChangeMainColor(CurrentColorName);
+    CurrentSlide.classList.remove("active");
+    prevSlide.classList.add("active");
+})
+// //////////////////////////////////////////////
+window.addEventListener("DOMContentLoaded",function(){
+    CarouselSliders[0].classList.add("active")
+})
+// /////////////////////////////
+
+window.addEventListener("load",()=>{
+    setTimeout(()=>{
+        LoadingPage.classList.add("hide");
+        document.body.classList.add("show")
+    } , 3000)
+})
+// /////////////////////////////////////////
+NavToggler.addEventListener("click",function(){
+    setTimeout(()=>{
+           if(NavMenu.classList.contains("show")){
+             GloabalCarouser.classList.add("blur-header");
+           }
+           else{
+             GloabalCarouser.classList.remove("blur-header");
+           }
+    } , 380)
+})
+// /////////////////////////////////
+NavToggler.addEventListener("click",function(){
+    setTimeout(()=>{
+           if(NavMenu.classList.contains("show")){
+             Offer.classList.add("blur-header");
+           }
+           else{
+             Offer.classList.remove("blur-header");
+           }
+    } , 380)
+})
+// //////////////////////////////////////////
+NavToggler.addEventListener("click",function(){
+    setTimeout(()=>{
+           if(NavMenu.classList.contains("show")){
+             LatestBlur.classList.add("blur-header");
+           }
+           else{
+             LatestBlur.classList.remove("blur-header");
+           }
+    } , 380)
+})
+// ///////////////////////////////////////
+NavToggler.addEventListener("click",function(){
+    setTimeout(()=>{
+           if(NavMenu.classList.contains("show")){
+             Featured.classList.add("blur-header");
+           }
+           else{
+             Featured.classList.remove("blur-header");
+           }
+    } , 380)
+})
+// ////////////////////////////////////////
+window.addEventListener("scroll",function(e){
+    if(window.scrollY > NavHeight){
+        NavItem.classList.add("scrolled");
+    }
+    else{
+        NavItem.classList.remove("scrolled");
+    }
+})
+// /////////////////////////////////
+NavLinks.forEach(function(NavLink){
+    NavLink.addEventListener("click",function(e){
+        e.preventDefault();
+    let LastLink = NavItem.querySelector(".nav-link.active"),
+        sectionId = NavLink.getAttribute("href"),
+        CurrentSection = document.querySelector(`${sectionId}`),
+        TopOfSection = CurrentSection.offsetTop ;
+
+    LastLink.classList.remove("active");
+    NavLink.classList.add("active");
+
+    window.scrollTo({
+        top : TopOfSection - NavHeight ,
+        left : 0 ,
+    })
+    })
+})
+
+// ///////////////////////////////////
+PopupBoxes.forEach(function(PopupBox){
+    PopupBox.addEventListener("click",function(e){
+        e.stopPropagation();
+    })
+})
+// /////////////////////////////////
+latest.forEach(function(product){
+
+    let ProductIntoCart = ProductCarts.find((item) => item.id == product.id);
+    console.log(ProductIntoCart);
+
+
+    LatestContent.innerHTML +=`
+     <div class="product px-4 pt-4 pb-3 rounded-3 mainborder mb-4"
+        data-product-id="${product.id}"
+        data-selected-color="${(ProductIntoCart == undefined) ? product.colors[0] : ProductIntoCart.color}"
+        data-selected-size="${(ProductIntoCart == undefined) ? product.sizes[0] : ProductIntoCart.size}">
+                  <div class="row">
+                    <div class="col-lg-6">
+                      <div class="item content">
+                        <div class="row">
+                          <div class="col-lg-2 part1">
+                             <div class="item">
+                                <ul class="list-unstyled box1 d-flex justify-content-center d-lg-block">
+                                  ${ShowImages(product.images)}
+                                </ul>
+                             </div>
+                          </div>
+                          <div class="col-lg-10 part2 box2">
+                            <div class="item .image">
+                                <div class="selsectedimg">
+                                  <img src="./images/products/${product.images[0]}" alt="" class="img-fluid">
+                                </div>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                    <div class="col-lg-6">
+                      <div class="item">
+                        <h2 class="maincolor">${product.name}</h2>
+                        <p>${product.description}</p>
+                        <div class="d-flex fw-bold">
+                          <div class="label me-2 ">
+                               <h5>Price :</h5>
+                          </div>
+                          <div class="value">
+                            ${showprice(product.price , product.discount)}
+                          </div>
+                        </div>
+
+                          <div class="d-flex fw-bold">
+                          <div class="label me-2 ">
+                               <h5>Size :</h5>
+                          </div>
+                          <div class="value">
+                               <ul class="list-unstyled d-flex">
+                                    ${ShowSizes(product.sizes , ProductIntoCart)}
+                               </ul>
+                          </div>
+                        </div>
+                        ${PrepareBtn(ProductIntoCart , product.id)}
+                     
+                      </div>
+                    </div>
+                  </div>
+                </div>
+    `;
+})
+// /////////////////////////////////
+features.forEach(function(product){
+    FeaturedContent.innerHTML += `
+       <div class="col-sm-6 col-md-4 col-lg-3 product text-center">
+                         <div class="item mb-3 rounded-3 py-3">
+                          <h6 class="discount">${showDiscount(product.discount)}</h6>
+                          <div class="selsectedimg">
+                            <img src="./images/products/${product.images[0]}" alt="" class="img-fluid mb-5">
+                          </div>
+                          <i class="fa-solid fa-magnifying-glass mt-5" onclick="OpenProductPopup(${product.id})"></i>
+                          <ul class="list-unstyled d-flex justify-content-center">
+                          ${ShowImgIcon(product.images)}
+                           
+                          </ul>
+                          <h5>${product.name}</h5>
+                          <div class="value fw-bold">
+                               ${showprice(product.price , product.discount)}
+                          </div>
+                         </div>
+                  </div>
+    `;
+})
+// /////////////////////////////////////
